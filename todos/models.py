25453b2a54bdece15d7fe9dfc3208196bcf85627
K-zis <zisios.kyriakos@gmail.com>
2024-05-27 23:15:16 +0300

    make content blank-able

--- todos/models.py
+++ todos/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Todo(models.Model):
     title = models.CharField(max_length=100)
-    content = models.TextField()
+    content = models.TextField(blank=True)
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="todos")
